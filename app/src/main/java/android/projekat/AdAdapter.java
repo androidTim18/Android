d4e775b5c276b0588a2dafa2830bafa85a2ce0b6
Smiljana <androidtim18@outlook.com>
2019-04-15 05:45:33 +0200

    Added: AddActivity, ListAll.java, ListFav.java, Edited: AdListActivity

---
+++ app/src/main/java/android/projekat/AdAdapter.java
@@ -0,0 +1,16 @@
+package android.projekat;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class ElementAdapter {
+}
---
+++ app/src/main/java/android/projekat/ListFav.java
@@ -0,0 +1,4 @@
+package android.projekat;
+
+public class ListFav {
+}
